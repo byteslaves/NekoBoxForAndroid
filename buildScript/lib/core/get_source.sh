8b1648fcde06156787df58fd054ef707bc9cd614
byteslaves <40004076+byteslaves@users.noreply.github.com>
2026-07-24 22:00:47 +0800

    Update git clone URL for sing-box repository

--- buildScript/lib/core/get_source.sh
+++ buildScript/lib/core/get_source.sh
@@ -9,7 +9,7 @@ pushd ..
 ####
 
 if [ ! -d "sing-box" ]; then
-  git clone --no-checkout https://github.com/starifly/sing-box.git
+  git clone --no-checkout https://github.com/byteslaves/sing-box.git
 fi
 pushd sing-box
 git checkout "$COMMIT_SING_BOX"
